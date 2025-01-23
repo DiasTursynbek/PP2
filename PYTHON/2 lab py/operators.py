c61c1576603cde10e354fae23a6d2387c1e3fbac
Dias-git <dias.tursinbek06@gmail.com>
2025-01-23 20:38:31 +0500

    PYTHON

---
+++ PYTHON/2 lab py/operators.py
@@ -0,0 +1,9 @@
+a = 16
+b = 4
+print('a + b = ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+print('a ** b = ', a ** b)
+print('a // b = ', a // b)       
+print('a % b = ', a % b)

c28d6278d1eb86ecbe0519a0f0f0980a641d0b3b
Dias-git <dias.tursinbek06@gmail.com>
2025-02-03 11:01:26 +0500

    py

--- PYTHON/3 lab py/string.py
+++ PYTHON/3 lab py/string.py
@@ -1,12 +1,12 @@
 class classss:
-    def __init__(self):
-        self.string = ""
+    def __init__(classss):
+        classss.string = ""
 
-    def getString(self):
-        self.string = input("Введите строку: ")
+    def getString(classss):
+        classss.string = input("Введите строку: ")
 
-    def printString(self):
-        print(self.string.upper())
+    def printString(classss):
+        print(classss.string.upper())
 
 obj = classss()
 obj.getString()

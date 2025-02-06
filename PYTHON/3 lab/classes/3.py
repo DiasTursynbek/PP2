c87916c7bdebd02d36f3afd1165fd8d532fbe3a8
Dias-git <dias.tursinbek06@gmail.com>
2025-02-06 22:26:21 +0500

    py

--- PYTHON/3 lab/classes/3.py
+++ PYTHON/3 lab/classes/3.py
@@ -1,19 +1,16 @@
 class Shape:
     def __init__(self):
-        pass  # Базовый класс, который пока не содержит функционала
+        pass  
 
-# Класс Rectangle наследует класс Shape
 class Rectangle(Shape):
     def __init__(self, length, width):
-        super().__init__()  # Вызываем __init__() родительского класса Shape
-        self.length = length  # Сохраняем длину прямоугольника
-        self.width = width  # Сохраняем ширину прямоугольника
+        super().__init__()  
+        self.length = length  
+        self.width = width  
 
     def area(self):
-        return self.length * self.width  # Вычисляем площадь прямоугольника (длина * ширина)
+        return self.length * self.width
 
-# Создаем объект прямоугольника с длиной 4 и шириной 5
 rect = Rectangle(4, 5)
 
-# Вызываем метод area() и печатаем результат
 print(rect.area())  
--- PYTHON/3 lab/classes/4.py
+++ PYTHON/3 lab/classes/4.py
@@ -1,22 +1,21 @@
-import math  # Импортируем модуль math для вычисления квадратного корня
-
+import math  
 class Point:
     def __init__(self, x, y):
-        self.x = x  # Координата x
-        self.y = y  # Координата y
+        self.x = x  
+        self.y = y  
 
     def show(self):
         print(f"({self.x}, {self.y})")
     def move(self, dx, dy):
-        self.x += dx  # Изменяем координату x
-        self.y += dy  # Изменяем координату y
+        self.x += dx 
+        self.y += dy 
 
     def dist(self, other_point):
         return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2)
 
-# Создание двух точек
-p1 = Point(1, 2)  # Точка с координатами (1,2)
-p2 = Point(4, 6)  # Точка с координатами (4,6)
 
-# Выводим расстояние между точками
-print(p1.dist(p2))  # Ожидаемый вывод: 5.0
+p1 = Point(1, 2) 
+p2 = Point(4, 6)  
+
+
+print(p1.dist(p2)) 

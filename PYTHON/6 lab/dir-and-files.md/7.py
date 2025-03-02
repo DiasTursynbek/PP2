cbc1a79ee7e1253240757d07ab974c81ef0c57ec
Dias-git <dias.tursinbek06@gmail.com>
2025-03-02 20:06:46 +0500

    update

--- PYTHON/6 lab/dir-and-files.md/7.py
+++ PYTHON/6 lab/dir-and-files.md/7.py
@@ -1,2 +1,2 @@
-with open("example.txt", "r") as src, open("destination.txt", "w") as dst: #Этот код копирует содержимое файла "example.txt" в новый файл "destination.txt".
+with open("example2.txt", "r") as src, open("destination.txt", "w") as dst: #Этот код копирует содержимое файла "example.txt" в новый файл "destination.txt".
     dst.write(src.read())
--- PYTHON/6 lab/dir-and-files.md/8.py
+++ PYTHON/6 lab/dir-and-files.md/8.py
@@ -1,6 +1,6 @@
 import os
 
-file_path = "example.txt"
+file_path = "examplefordelete.txt"
 
 if os.path.exists(file_path) and os.access(file_path, os.W_OK):
     os.remove(file_path)
